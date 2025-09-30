954bf82040418a7c15a8524e98aa43326bad2175
Rishabh Raj <rajrishabh2002711@gamil.com>
2025-09-30 21:59:43 +0530

    add schema of tables

---
+++ schema.sql
@@ -0,0 +1,50 @@
+CREATE TABLE users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE stocks (
+    stock_id INT AUTO_INCREMENT PRIMARY KEY,
+    symbol VARCHAR(10) UNIQUE NOT NULL,
+    company_name VARCHAR(100) NOT NULL,
+    current_price DECIMAL(10,2) NOT NULL
+);
+
+CREATE TABLE transactions (
+    transaction_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    stock_id INT,
+    type ENUM('BUY','SELL') NOT NULL,
+    quantity INT NOT NULL,
+    price DECIMAL(10,2) NOT NULL, -- price at transaction time
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (stock_id) REFERENCES stocks(stock_id)
+);
+
+
+CREATE TABLE holdings (
+    holding_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    stock_id INT,
+    quantity INT NOT NULL,
+    avg_price DECIMAL(10,2) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (stock_id) REFERENCES stocks(stock_id),
+    UNIQUE(user_id, stock_id) -- one record per stock per user
+);
+
+
+CREATE TABLE watchlist (
+    watch_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    stock_id INT,
+    added_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (stock_id) REFERENCES stocks(stock_id),
+    UNIQUE(user_id, stock_id)
+);
